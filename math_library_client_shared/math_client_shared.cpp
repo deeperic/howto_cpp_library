386aae07f7ef562e49ca48813c6025637feb68bb
Eric Chan <ceric318@gmail.com>
2024-02-20 21:21:20 +0800

    first commit

---
+++ math_library_client_shared/math_client_shared.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+#include "mathlib.h"
+
+int main()
+{
+	std::cout << "Client linked with shared library" << std::endl;
+	auto sum = MathLibrary::Arithmetic::Add(3, 7);
+	std::cout << "result: " << sum << std::endl;
+
+	return 0;
+}
---
+++ math_library_client_static/math_client_static.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+#include "mathlib.h"
+
+int main()
+{
+	std::cout << "Client linked with static library." << std::endl;
+	auto product = MathLibrary::Arithmetic::Multiply(3, 7);
+    std::cout << "result: " << product << std::endl;
+
+	return 0;
+}
